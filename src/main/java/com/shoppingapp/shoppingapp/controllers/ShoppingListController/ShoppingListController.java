996a2d615ccab02b4e4d2a57145e5b4b997811e2
nguyand2 <nguyand2@oregonstate.edu>
2021-04-06 14:12:25 -0700

    Added test endpoints for GET and POST, dependencies for aws sdk java

--- src/main/java/com/shoppingapp/shoppingapp/controllers/ShoppingListController/ShoppingListController.java
+++ src/main/java/com/shoppingapp/shoppingapp/controllers/ShoppingListController/ShoppingListController.java
@@ -1,13 +1,26 @@
 package com.shoppingapp.shoppingapp.controllers.ShoppingListController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.amazonaws.services.rds.AmazonRDSClientBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class ShoppingListController {
 
-    @GetMapping(value={"/", "/home"})
+    // would add into an aws builder after test connection
+
+    @GetMapping(value = {"/", "/home"})
+    @ResponseStatus(HttpStatus.OK)
     public String home() {
+        AmazonRDSClientBuilder clientBuilder;
+        // RDS instance created with table Items
         return "home";
+    }
+
+    @PostMapping("/compare")
+    public String compareShoppingList(@RequestBody(required=true) Map<String, Object>  payload) {
+        return payload.toString();
     }
 }
