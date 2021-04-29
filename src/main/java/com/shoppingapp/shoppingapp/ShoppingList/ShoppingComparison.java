24de5bd9f7e7d9dcf9a5194a98ccf39b6cd94a0e
nguyand2 <nguyand2@oregonstate.edu>
2021-04-29 15:23:12 -0700

    Added fields for mvp of price and store info into the product, added base shopping comparison impl

--- src/main/java/com/shoppingapp/shoppingapp/ShoppingList/ShoppingComparison.java
+++ src/main/java/com/shoppingapp/shoppingapp/ShoppingList/ShoppingComparison.java
@@ -6,8 +6,8 @@ import com.shoppingapp.shoppingapp.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @AllArgsConstructor
@@ -17,21 +17,33 @@ public class ShoppingComparison {
     private EligibleZips eligibleZips;
     private ProductRepository productRepository;
 
-    public String getChosenList(String payload) {
+    public Shopping_Info getChosenList(String payload) {
         Shopping_Info shopping_info = compareItems(shoppingInfoExtractor.processPayload(payload));
-        return shopping_info.toString();
+        return shopping_info;
     }
 
     private Shopping_Info compareItems(Shopping_Info shopping_info) {
         // go through each item of the shoppingList, get the name
-        Map<String, Float> shopping_items = shopping_info.getShoppingList();
-        shopping_items.forEach((String item, Float price) -> {
-            // query to get the Item based on the name
-            System.out.println(item);
-            System.out.println(price);
-            List<Product> candidate_items = productRepository.findAllProductsWithName(item);
-        });
+        ArrayList<Map<String, Map<String, Double>>> list = shopping_info.getShoppingList();
+        // top level is "item_name" and then add the "chosenProduct" object
+
+        ArrayList<Product> shoppingItems = shoppingInfoExtractor.processItems(shopping_info.getShoppingList());
         // for the query could either use the eligibleZips before or after the comparison query
+        AtomicInteger index = new AtomicInteger(0);
+        shoppingItems.forEach(product -> {
+            Map<String, Double> createdItem = new HashMap<>();
+            List<Product> candidate_items = productRepository.findAllProductsWithName(product.getItem_name());
+            if (candidate_items.size() <= 0) {
+                // no alternative
+                // put "no alternative
+                createdItem.put("no cheaper alternative", 0.0);
+            } else{
+                Collections.sort(candidate_items, (Comparator.comparingDouble(Product::getPrice)));
+                createdItem.put(candidate_items.get(0).getItem_name(), candidate_items.get(0).getPrice());
+            }
+            list.get(index.intValue()).put("chosenProduct", createdItem);
+            index.addAndGet(1);
+        });
         // until the eligibleZips are in here, then will do it after
         return shopping_info;
     }
--- src/main/java/com/shoppingapp/shoppingapp/ShoppingList/ShoppingInfoExtractor.java
+++ src/main/java/com/shoppingapp/shoppingapp/ShoppingList/ShoppingInfoExtractor.java
@@ -4,23 +4,52 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.shoppingapp.shoppingapp.model.Product;
 import com.shoppingapp.shoppingapp.model.Shopping_Info;
 import com.shoppingapp.shoppingapp.model.Shopping_List;
 import org.joda.time.IllegalFieldValueException;
 import org.springframework.data.util.Optionals;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class ShoppingInfoExtractor {
 
+    Gson gson = new Gson();
+
     public Shopping_Info processPayload(String payload) throws IllegalFieldValueException {
-        Gson gson = new Gson();
+        System.out.println(payload);
         Shopping_Info shopping_info = gson.fromJson(payload, Shopping_Info.class);
+        System.out.println(shopping_info);
         validateRequiredFields(shopping_info);
         return shopping_info;
+    }
+
+    public ArrayList<Product> processItems(ArrayList<Map<String, Map<String, Double>>> items) {
+        ArrayList<Product> processedItems = new ArrayList<>();
+        items.forEach(item-> {
+                String name = retrieveName(item);
+                JsonElement jsonItem = gson.toJsonTree(item.get(name));
+
+                Product product = gson.fromJson(jsonItem, Product.class);
+                product.setItem_name(name);
+                processedItems.add(product);
+            }
+        );
+        return processedItems;
+    }
+
+    private String retrieveName(Map<String, Map<String, Double>> singleItem) {
+        for (String key: singleItem.keySet()) {
+            // only key for the item is the name
+            return key;
+        }
+        return null;
     }
 
     private void validateRequiredFields(Shopping_Info payload) throws IllegalFieldValueException {
