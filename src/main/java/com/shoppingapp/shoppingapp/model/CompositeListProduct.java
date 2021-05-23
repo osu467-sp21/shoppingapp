84e7d0228b262800123f8917e5d56eb0aeebeef4
Cameron Grover <38637240+CamGrover@users.noreply.github.com>
2021-05-23 12:18:57 -0500

    Rename List_Items.java to List_Product.java. Add CompositeListProduct.java and ListProductRepository.java.

---
+++ src/main/java/com/shoppingapp/shoppingapp/model/CompositeListProduct.java
@@ -0,0 +1,8 @@
+package com.shoppingapp.shoppingapp.model;
+
+import java.io.Serializable;
+
+public class CompositeListProduct implements Serializable {
+    private Long shopping_list_id;
+    private Long product_id;
+}
