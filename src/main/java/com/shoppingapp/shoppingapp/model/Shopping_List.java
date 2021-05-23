7b295837c6ce2aaa43726c610bfeb5a7c6212268
Cameron Grover <38637240+CamGrover@users.noreply.github.com>
2021-05-23 12:26:10 -0500

    Rename List_Item.java to List_Product.java. Add shopping_list_id to Shopping_List.

--- src/main/java/com/shoppingapp/shoppingapp/model/Shopping_List.java
+++ src/main/java/com/shoppingapp/shoppingapp/model/Shopping_List.java
@@ -4,9 +4,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.Value;
 
+import javax.persistence.Id;
+
 @Data
 @Builder
 @Value
 public class Shopping_List {
+    @Id
+    String shopping_list_id;
     String user_id; // foreign key
 }
